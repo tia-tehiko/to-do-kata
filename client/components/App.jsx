2c0dc384e27273dff857e7794211b51c253e0b7e
Tia TeHiko <tiatehiko@gmail.com>
2020-10-22 10:07:57 +1000

    tasks setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+const tasks = [
+  { id: 1, name: 'update portfolio' },
+  { id: 2, name: 'apply for jobs' },
+  { id: 3, name: 'get new car' }
+]
+
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map(task => <li key={task.id}>{task.name}</li>)}
+      </ul>
+
+    </>
   )
 }
 
