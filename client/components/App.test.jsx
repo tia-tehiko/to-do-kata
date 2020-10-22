b94f7491785d4f2241d1cb33dd87d482fb62dffd
Tia TeHiko <tiatehiko@gmail.com>
2020-10-22 16:57:10 +1000

    test App component

--- client/components/App.test.jsx
+++ client/components/App.test.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import store from '../store'
+
+jest.spyOn(store, 'getState')
+
+beforeEach(() => {
+  store.getState.mockImplementation(() => ({
+    tasks: [
+      { id: 1, name: 'update portfolio' },
+      { id: 2, name: 'do washing' },
+      { id: 3, name: 'ring mum' }
+    ]
+  }))
+})
 
 test('displays a task list', () => {
-  render(<App />)
+  render(<Provider store={store}><App /></Provider>)
   expect.assertions(2)
   const tasks = screen.getAllByRole('listitem')
   expect(tasks).toHaveLength(3)
