05758371b81cc99c57f055407c2622895b6e4059
Tia TeHiko <tiatehiko@gmail.com>
2020-10-22 16:56:49 +1000

    export tasks to reducer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const tasks = [
-  { id: 1, name: 'update portfolio' },
-  { id: 2, name: 'do washing' },
-  { id: 3, name: 'ring mum' }
-]
-
-const App = () => {
-  return (
-    <>
-      <h1>Tasks</h1>
-      <ul>
-        {tasks.map(task => <li key={task.id}>{task.name}</li>)}
-      </ul>
-
-    </>
-  )
+export class App extends React.Component {
+  render() {
+    let { tasks } = this.props
+    return (
+      <>
+        <h1>Tasks</h1>
+        <ul>
+          {tasks.map(task => <li key={task.id}>{task.name}</li>)}
+        </ul>
+      </>
+    )
+  }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps)(App)
