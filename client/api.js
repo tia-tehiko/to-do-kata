9876d27fa1eae17a5a6c9a1e088a9e45ebe6757f
Tia TeHiko <tiatehiko@gmail.com>
2020-10-22 19:33:20 +1000

    setup inital api

---
+++ client/api.js
@@ -0,0 +1,8 @@
+export function fetchTasks() {
+  const tasks = [
+    { id: 1, name: 'update portfolio' },
+    { id: 2, name: 'do washing' },
+    { id: 3, name: 'ring mum' }
+  ]
+  return Promise.resolve(tasks)
+}
