b92b8d83f9042bcaf87beafaadc82dd3122c11b1
Tia TeHiko <tiatehiko@gmail.com>
2020-10-23 15:14:46 +1000

    add catch err to route

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,9 @@ const { getTasks } = require('./db')
 router.get('/', (req, res) => {
   return getTasks()
     .then((tasks) => res.json(tasks))
+    .catch(err => {
+      res.status(500).send(err)
+    })
 })
 
 module.exports = router
