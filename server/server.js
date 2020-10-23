e424a0beb628f33a6b88e06fb8b692589aa059ec
Tia TeHiko <tiatehiko@gmail.com>
2020-10-23 13:24:55 +1000

    fetch tasks from server through api

--- server/server.js
+++ server/server.js
@@ -6,4 +6,13 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+server.get('/api/v1/tasks', (req, res) => {
+  const tasks = [
+    { id: 1, name: 'update portfolio' },
+    { id: 2, name: 'do washing' },
+    { id: 3, name: 'ring mum' }
+  ]
+  res.json(tasks)
+})
+
 module.exports = server
