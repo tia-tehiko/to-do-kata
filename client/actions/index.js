bf28078c2ff380e7e29b735aef510979b3008f11
Tia TeHiko <tiatehiko@gmail.com>
2020-10-24 21:57:36 +1000

    create ADD_TASK action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const SET_TASKS = 'SET_TASKS'
+export const ADD_TASKS = 'ADD_TASKS'
 
 export function setTasks (tasks) {
   return ({
     type: SET_TASKS,
     tasks
+  })
+}
+
+export function addTasks (task) {
+  return ({
+    type: ADD_TASKS,
+    task
   })
 }
