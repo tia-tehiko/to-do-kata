743569eaac8eb803205485e11218b8b288c825e5
Tia TeHiko <tiatehiko@gmail.com>
2020-10-24 14:20:54 +1000

    import AddTask to App and test

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+
 import Tasks from './Tasks'
+import AddTask from './AddTask'
 
 class App extends React.Component {
   render () {
     return (
       <>
         <Tasks />
+        <AddTask />
       </>
     )
   }
--- client/components/App.test.jsx
+++ client/components/App.test.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
+import store from '../store'
 import App from './App'
 
-jest.mock('./Tasks.jsx', () => {
-  return () => <h1>Task List</h1>
-})
+jest.mock('./Tasks.jsx', () => { return () => <h1>Task List</h1> })
+jest.mock('./AddTask.jsx', () => { return () => <h1>Add New Task</h1> })
 
 test('renders tasks', () => {
-  render(<App />)
-  const heading = screen.getByRole('heading')
-  expect(heading.innerHTML).toMatch(/Task List/)
+  render(<Provider store={store}><App /></Provider>)
+  const heading = screen.getAllByRole('heading')
+  expect(heading[0].innerHTML).toMatch(/Task List/)
+})
+
+test('renders add task', () => {
+  render(<Provider store={store}><App /></Provider>)
+  const heading = screen.getAllByRole('heading')
+  expect(heading[1].innerHTML).toMatch('Add New Task')
 })
