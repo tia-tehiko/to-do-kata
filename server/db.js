b070b68e37b2c487cd889f0eca76b858cadd4109
Tia TeHiko <tiatehiko@gmail.com>
2020-10-24 17:29:23 +1000

    create saveTask db function and test

--- server/db.js
+++ server/db.js
@@ -3,10 +3,16 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 const connection = knex(config)
 
-function getTasks (db = connection) {
+function getTasks(db = connection) {
   return db('tasks').select()
 }
 
+function saveTask({ name }, db = connection) {
+  return db('tasks')
+    .insert({ name })
+}
+
 module.exports = {
-  getTasks
+  getTasks,
+  saveTask
 }
--- server/db.test.js
+++ server/db.test.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const config = require('../knexfile').test
 const db = knex(config)
 
-const { getTasks } = require('./db')
+const { getTasks, saveTask } = require('./db')
 
 beforeAll(() => db.migrate.latest())
 beforeEach(() => db.seed.run())
@@ -11,6 +11,19 @@ test('get all tasks', () => {
   return getTasks(db)
     .then((tasks) => {
       expect(tasks).toHaveLength(3)
+      return null
+    })
+})
+
+test('save task', () => {
+  return saveTask({ name: 'new task' }, db)
+    .then(ids => {
+      expect(ids[0] > 0).toBe(true)
+      return getTasks(db)
+    })
+    .then(tasks => {
+      expect(tasks.length).toBe(4)
+      expect(tasks[3].name).toBe('new task')
       return null
     })
 })
