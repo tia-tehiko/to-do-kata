6382eae31f41d2299520a19c920505012814693d
Tia TeHiko <tiatehiko@gmail.com>
2020-10-26 21:30:31 +1000

    create and test db functions for delete task

--- server/db.js
+++ server/db.js
@@ -3,16 +3,24 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 const connection = knex(config)
 
-function getTasks (db = connection) {
+function getTasks(db = connection) {
   return db('tasks').select()
 }
 
-function saveTask ({ name }, db = connection) {
+function saveTask({ name }, db = connection) {
   return db('tasks')
     .insert({ name })
 }
 
+function deleteTask(id, db = connection) {
+  if (!id) return Promise.reject('id must be specified')
+  return db('tasks')
+    .where('id', id)
+    .del()
+}
+
 module.exports = {
   getTasks,
-  saveTask
+  saveTask,
+  deleteTask
 }
--- server/db.test.js
+++ server/db.test.js
@@ -2,12 +2,13 @@ const knex = require('knex')
 const config = require('../knexfile').test
 const db = knex(config)
 
-const { getTasks, saveTask } = require('./db')
+const { getTasks, saveTask, deleteTask } = require('./db')
 
 beforeAll(() => db.migrate.latest())
 beforeEach(() => db.seed.run())
 
 test('get all tasks', () => {
+  expect.assertions(1)
   return getTasks(db)
     .then((tasks) => {
       expect(tasks).toHaveLength(3)
@@ -16,6 +17,7 @@ test('get all tasks', () => {
 })
 
 test('save task', () => {
+  expect.assertions(3)
   return saveTask({ name: 'new task' }, db)
     .then(ids => {
       expect(ids[0] > 0).toBe(true)
@@ -26,4 +28,33 @@ test('save task', () => {
       expect(tasks[3].name).toBe('new task')
       return null
     })
+})
+
+
+
+describe('delete', () => {
+  test('deletes a task', () => {
+    expect.assertions(2)
+    return deleteTask(1, db)
+      .then(deletedTask => {
+        expect(deletedTask).toBe(1)
+        return getTasks(db)
+      })
+      .then(tasks => {
+        expect(tasks.length).toBe(2)
+        return null
+      })
+  })
+  test('id of undefined doesnt break anything', () => {
+    expect.assertions(2)
+    return deleteTask(undefined, db)
+      .catch(errorMessage => {
+        expect(errorMessage).toBe('id must be specified')
+        return getTasks(db)
+      })
+      .then(tasks => {
+        expect(tasks.length).toBe(3)
+        return null
+      })
+  })
 })
