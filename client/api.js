ca1e88e9139d245e60f7534174684a07f6c90fd1
Tia TeHiko <tiatehiko@gmail.com>
2020-10-26 20:54:13 +1000

    api client and tests for delete task

--- client/api.js
+++ client/api.js
@@ -1,14 +1,20 @@
 import request from 'superagent'
 
-export function fetchTasks () {
+export function fetchTasks() {
   return request
     .get('/api/v1/tasks')
     .then(res => res.body)
 }
 
-export function addTask (name) {
+export function addTask(name) {
   return request
     .post('/api/v1/tasks')
     .send({ name })
     .then(res => res.body.id)
+}
+
+export function deleteTask(id) {
+  return request
+    .delete('/api/v1/tasks/' + id)
+    .then(res => res.body)
 }
